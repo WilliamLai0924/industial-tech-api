b8e8fa63b1d2b47bf6fdf79a8e272d3c32b87882
YU <w25225575@gmail.com>
2024-10-05 13:47:43 +0800

    fix bug

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def handle_file_message(event):
         try:            
             messages = []
             messages.append('SS')
-            messages.append(str(file_bytes.read()))
+            # messages.append(str(file_bytes.read()))
             # data = evalue_plan.get_excel_data(file_bytes)
             # messages.append(str(len(data)))
             # filter_df = evalue_plan.filter_valid_data(data)   
